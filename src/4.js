5016ffbbdeaa77ef7ea6fa33dcb6e1ddaf2216ef
bryanaklev1ui <bryanaklev1ui@gmx.com>
2025-03-07 13:44:52 +0000

    Add 4.js

---
+++ src/4.js
@@ -0,0 +1,7 @@
+// Generate a random number between 1 and 10
+function getRandomNumber() {
+  return Math.floor(Math.random() * 10) + 1;
+}
+
+// Print the random number to the console
+console.log(getRandomNumber());
