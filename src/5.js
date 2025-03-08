aa55c1432298b50260c399edc9e6f4e65f74fb89
bryanaklev1ui <bryanaklev1ui@gmx.com>
2025-03-08 06:44:45 +0000

    Add 5.js

---
+++ src/5.js
@@ -0,0 +1,7 @@
+// Generate a random number between 1 and 100
+function getRandomNumber() {
+  return Math.floor(Math.random() * 100) + 1;
+}
+
+// Print the generated number to the console
+console.log(getRandomNumber());
